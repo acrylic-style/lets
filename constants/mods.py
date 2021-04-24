c12882105fdb88d2d3c02c42100d481e2c4dc299
acrylic-style <ilyy@outlook.jp>
2021-04-24 14:40:24 +0900

    sure

--- constants/mods.py
+++ constants/mods.py
@@ -1,6 +1,6 @@
 from common.constants import mods
 
-def getModsForPP(gm: int):
+def getModsList(gm: int):
   # ignored mods: Touch device, Relax, Auto Pilot, Auto, Sudden Death, Perfect
   lst = []
   if gm == 0:
@@ -47,3 +47,54 @@ def getModsForPP(gm: int):
     lst.append("3K")
   if gm & mods.KEY2 > 0:
     lst.append("2K")
+  return lst
+
+def getModsForPP(gm: int):
+  # ignored mods: Touch device, Relax, Auto Pilot, Auto, Sudden Death, Perfect
+  s = ""
+  if gm == 0:
+    return s
+  if gm & mods.NOFAIL > 0:
+    s += "-m NF "
+  if gm & mods.EASY > 0:
+    s += "-m EZ "
+  if gm & mods.HIDDEN > 0:
+    s += "-m HD "
+  if gm & mods.HARDROCK > 0:
+    s += "-m HR "
+  if gm & mods.DOUBLETIME > 0:
+    s += "-m DT "
+  if gm & mods.HALFTIME > 0:
+    s += "-m HT "
+  if gm & mods.NIGHTCORE > 0:
+    s += "-m NC "
+  if gm & mods.FLASHLIGHT > 0:
+    s += "-m FL "
+  if gm & mods.SPUNOUT > 0:
+    s += "-m SO "
+  if gm & mods.KEY4 > 0:
+    s += "-m 4K "
+  if gm & mods.KEY5 > 0:
+    s += "-m 5K "
+  if gm & mods.KEY6 > 0:
+    s += "-m 6K "
+  if gm & mods.KEY7 > 0:
+    s += "-m 7K "
+  if gm & mods.KEY8 > 0:
+    s += "-m 8K "
+  if gm & mods.FADEIN > 0:
+    s += "-m FI "
+  if gm & mods.RANDOM > 0: # Random scores shouldn't be submitted (and its unranked mod)
+    s += "-m RD "
+  if gm & mods.KEY9 > 0:
+    s += "-m 9K "
+  if gm & mods.KEY10 > 0:
+    s += "-m 10K "
+  if gm & mods.KEY1 > 0:
+    s += "-m 1K "
+  if gm & mods.KEY3 > 0:
+    s += "-m 3K "
+  if gm & mods.KEY2 > 0:
+    s += "-m 2K "
+  return s
+
