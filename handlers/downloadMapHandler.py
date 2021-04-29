e54b04eeaa3d4037930cb4c2fd5a54308940bf47
acrylic-style <ilyy@outlook.jp>
2021-04-29 20:29:47 +0900

    ok so we need to say eternal thanks to chimu.moe
    i would donate if i was able to (patreon denies my card)

--- handlers/downloadMapHandler.py
+++ handlers/downloadMapHandler.py
@@ -1,13 +1,9 @@
 import requests
 import tornado.gen
 import tornado.web
-import time
-import hashlib
 
 from common.web import requestsManager
 from common.sentry import sentry
-from objects import glob
-import urllib.parse
 
 class handler(requestsManager.asyncRequestHandler):
 	"""
@@ -25,28 +21,9 @@ class handler(requestsManager.asyncRequestHandler):
 				bid = bid[:-1]
 			bid = int(bid)
 
-			res = glob.db.fetch("SELECT * FROM osu_beatmapsets WHERE beatmapset_id = %s LIMIT 1", bid)
-			if res is None:
-				raise ValueError()
-			nv = "1" if noVideo else "0"
-			artist = res["artist"]
-			title = res["title"]
-			diskFilename = ""
-			serveFilename = f"{bid} {artist} - {title}"
-			if noVideo:
-				serveFilename += " [no video]"
-			serveFilename += ".osz"
-			serveFilename = serveFilename.replace('"', '').replace('?', '')
-			currentTime = int(time.time())
-			checksum = hashlib.md5(f"{bid}{diskFilename}{serveFilename}{currentTime}{nv}a".encode()).hexdigest()
-			eServeFilename = urllib.parse.quote_plus(serveFilename).replace("+", "%20")
-			url = f"https://osu.ppy.sh/d/{bid}?fs={eServeFilename}&fd={diskFilename}&ts={currentTime}&cs={checksum}&nv={nv}"
-			#response = requests.get(url, timeout=5)
-			#self.write(response.content)
-			self.set_status(302, "Moved Temporarily")
-			self.add_header("Location", url)
-			self.add_header("Cache-Control", "no-cache")
-			self.add_header("Pragma", "no-cache")
+			nv = "0" if noVideo else "1"
+			response = requests.get(f"https://api.chimu.moe/v1/download/{bid}?n={nv}", timeout=5)
+			self.write(response.content)
 		except ValueError:
 			self.set_status(400)
 			self.write("Invalid set id")
